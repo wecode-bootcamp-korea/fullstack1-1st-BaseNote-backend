94f51b7f6cef77c5f4d2ce6dac51b9360f7d986a
Hayoung Gam <hahhyoung@gmail.com>
2021-07-11 18:37:29 +0900

    Add: ID 존재 확인, hashed PW와 플레인PW 비교

--- services/UserService.js
+++ services/UserService.js
@@ -1,9 +1,24 @@
 import { UserDao } from '../models';
-
-const { JWT_SECRET_KEY } = process.env;
+import bcrypt from 'bcrypt';
 
 const logIn = async (user_account, password) => {
   const userInfo = await UserDao.getUser(user_account);
+  let err;
+
+  if (!userInfo.length) {
+    err = new Error(`COULDN'T_FIND_YOUR_ACCOUNT.`);
+    err.statusCode = 404;
+    throw err;
+  }
+
+  const { user_account: id, password: hashedPassword } = userInfo[0];
+  const isMatch = await bcrypt.compare(password, hashedPassword);
+
+  if (!isMatch) {
+    err = new Error('INCORRECT_PASSWORD.');
+    err.statusCode = 401;
+    throw err;
+  }
 };
 
 export default { logIn };
