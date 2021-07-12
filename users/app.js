aa15c34387b21e65f7c7466bf800f82b122f4c41
undefinedP <kzbaram@gmail.com>
2021-07-12 19:59:29 +0900

    ADD: signup api in progress

--- users/app.js
+++ users/app.js
@@ -3,9 +3,12 @@ import routes from './routes';
 import cors from 'cors';
 
 const app = express();
+const logger = morgan('dev');
+console.log(routes);
 
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 app.use(routes);
 
 export default app;
--- users/server.js
+++ users/server.js
@@ -3,10 +3,14 @@ import app from './app';
 import prisma from '../prisma';
 
 const server = http.createServer(app);
+console.log(server);
 const PORT = process.env.PORT;
+
+console.log(app);
 
 const start = async () => {
   try {
+    console.log('엥', app);
     server.listen(PORT, () => console.log(`SERVER is listening on ${PORT}!!`));
   } catch (err) {
     console.log(err);
