a048ba1c884a96d86b3ebcb2f40b0d0916783508
Hayoung Gam <hahhyoung@gmail.com>
2021-07-13 03:40:22 +0900

    Add: 토큰 인가 미들웨어 구현

--- app.js
+++ app.js
@@ -9,4 +9,10 @@ app.use(cors());
 app.use(express.json());
 app.use(logger);
 
+app.use((err, req, res, next) => {
+  const { status, message } = err;
+  console.error(err);
+  res.status(status || 500).json({ message });
+});
+
 export default app;
