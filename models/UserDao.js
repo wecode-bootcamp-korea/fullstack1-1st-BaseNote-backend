9755244841ce4b184b35945440feb36f698736ec
Hayoung Gam <hahhyoung@gmail.com>
2021-07-13 01:20:56 +0900

    Refactor: getUser Dao의 select문 컬럼 기재

--- models/UserDao.js
+++ models/UserDao.js
@@ -2,7 +2,7 @@ import prisma from '../prisma';
 
 const getUser = async (user_account) => {
   return await prisma.$queryRaw(`
-    SELECT * FROM users WHERE user_account='${user_account}';
+    SELECT id, user_account, password FROM users WHERE user_account='${user_account}';
   `);
 };
 
