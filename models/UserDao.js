285ce3192d89370025753717e5a76a1455249674
Hayoung Gam <hahhyoung@gmail.com>
2021-07-13 12:09:41 +0900

    Remove: 콘솔로그 삭제

--- models/UserDao.js
+++ models/UserDao.js
@@ -1,7 +1,6 @@
 import prisma from '../prisma';
 
 const findUserId = async (id) => {
-  console.log(id);
   return await prisma.$queryRaw(`
     SELECT id, password FROM users WHERE id='${id}';
   `);
