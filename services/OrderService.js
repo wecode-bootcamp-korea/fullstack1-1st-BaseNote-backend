f19056bac6ab5b0a7a8e55c828f8979f14dfd97b
Hayoung Gam <hahhyoung@gmail.com>
2021-07-15 02:53:36 +0900

    Add: Cart Item quantity Update service 작성

--- services/OrderService.js
+++ services/OrderService.js
@@ -4,4 +4,8 @@ const findCartItems = async (id) => {
   return await OrderDao.findCartItems(id);
 };
 
-export default { findCartItems };
+const updateCartItemQuantity = async (id, cart_item_id, quantity) => {
+  return await OrderDao.updateCartItemQuantity(id, cart_item_id, quantity);
+};
+
+export default { findCartItems, updateCartItemQuantity };
