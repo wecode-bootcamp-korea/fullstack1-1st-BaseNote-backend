b55d2e4b1cd0d34d55e15cf899b9684ca3b9daf5
Hayoung Gam <hahhyoung@gmail.com>
2021-07-14 22:10:56 +0900

    Add: 로그인한 user의 장바구니 항목을 select하는 findCartItems Dao 생성

---
+++ models/OrderDao.js
@@ -0,0 +1,32 @@
+import prisma from '../prisma';
+
+const findCartItems = async (id) => {
+  return await prisma.$queryRaw(`
+      SELECT
+        carts.id as cart_item_id,
+        users.id as user_id,
+        products.id,
+        products.name,
+        images.image_url,
+        series.name as series,
+        products.series_number,
+        volumes.volume_ml,
+        volumes.price
+      FROM carts
+        JOIN users
+          ON carts.user_id = users.id
+        JOIN products_volume
+          ON carts.product_volume_id = products_volume.id
+        JOIN products
+          ON products.id = products_volume.product_id
+        JOIN images
+          ON images.product_id = products.id
+        JOIN series
+          ON series.id = products.series_id
+        JOIN volumes
+          ON volumes.id = products_volume.volume_id
+      WHERE carts.user_id = '${id}';
+    `);
+};
+
+export default { findCartItems };
