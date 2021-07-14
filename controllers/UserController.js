501fb64f8c1847d5b30c9a1d0d1e022df6e34d88
undefinedP <kzbaram@gmail.com>
2021-07-14 22:11:33 +0900

    DELETE : console.log 삭제

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,7 +6,6 @@ const signUp = async (req, res) => {
 
     await UserService.signUp(name, email, user_account, phone_number, password);
 
-    console.log({ user_account });
     res.status(201).json({ user_account });
   } catch (err) {
     res.status(500).json({ message: err.message });
