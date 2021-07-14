8f76b2f7c6f0aeae5d5977161681757b55e12288
Hayoung Gam <hahhyoung@gmail.com>
2021-07-15 02:52:54 +0900

    Add: cart item quantity 업데이트 controller 작성

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -13,4 +13,32 @@ const findCartItems = async (req, res) => {
   }
 };
 
-export default { findCartItems };
+const updateCartItemQuantity = async (req, res) => {
+  try {
+    const { id } = req.foundUser[0];
+    const { cart_item_id, quantity } = req.body;
+    let err;
+
+    if (!cart_item_id && !quantity) {
+      err = new Error('INVALID_CART_ITEM');
+      err.statusCode = 400;
+      throw err;
+    } else if (!cart_item_id) {
+      err = new Error('INVALID_CART_ITEM');
+      err.statusCode = 400;
+      throw err;
+    } else if (!quantity) {
+      err = new Error('PLEASE_SET_THE_QUANTITY');
+      err.statusCode = 400;
+      throw err;
+    } else {
+      await OrderService.updateCartItemQuantity(id, cart_item_id, quantity);
+
+      res.status(200).json({ message: 'CART_ITEM_QUANTITY_UPDATED' });
+    }
+  } catch (err) {
+    res.status(err.statusCode || 500).json({ message: err.message });
+  }
+};
+
+export default { findCartItems, updateCartItemQuantity };
