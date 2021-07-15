72abb92038c619fdddb727f614ee51c110c06199
Hayoung Gam <hahhyoung@gmail.com>
2021-07-16 05:31:53 +0900

    Add: product router 작성

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import UserRouter from './UserRouter';
 import OrderRouter from './OrderRouter';
+import ProductRouter from './ProductRouter';
 
 const router = express.Router();
+
 router.use('/member', UserRouter);
 router.use('/order', OrderRouter);
+router.use('/product', ProductRouter);
 
 export default router;
