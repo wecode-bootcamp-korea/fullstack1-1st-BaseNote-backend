73df93c56180b22f066119c5cc768be3c91759b0
undefinedP <kzbaram@gmail.com>
2021-07-15 13:46:09 +0900

    list api in progress

--- models/listDao.js
+++ models/listDao.js
@@ -3,8 +3,13 @@ import prisma from '../prisma';
 const showLists = async () => {
   const products = await prisma.$queryRaw(`
     SELECT
-      products.id
-      products.name
-      products.
+      products.id,
+      products.name,
+      products.price,
+      image.image_url
+    FROM
+      images
+    JOIN products
+      ON images.product_id = products.id
   `);
 };
