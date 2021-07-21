b1bae9e97697ebdbbf716574bc49a807453ac37f
undefinedP <kzbaram@gmail.com>
2021-07-21 10:15:27 +0900

    ADD : Dao 파일에 필터링을 위한 sql문 추가

--- models/ProductDao.js
+++ models/ProductDao.js
@@ -18,8 +18,35 @@ const findProducts = async (volume) => {
         ON series.id = products.series_id
       JOIN images
         ON images.product_id = products.id
-    WHERE volumes.volume_ml = '${volume}';
+    WHERE volumes.volume_ml = '${volume}' 
   `);
 };
 
-export default { findProducts };
+const filterProducts = async (volume, scent) => {
+  return prisma.$queryRaw(`
+    SELECT
+      products.id,
+      products.name,
+      series.name as series,
+      products.series_number as seriesNumber,
+      images.image_url as imageURL,
+      volumes.price,
+      scents.name as scents
+    FROM products
+      JOIN products_volume
+        ON products.id = products_volume.product_id
+      JOIN volumes
+        ON volumes.id = products_volume.volume_id
+      JOIN series
+        ON series.id = products.series_id
+      JOIN images
+        ON images.product_id = products.id
+      JOIN scent_products
+        ON products.id = scent_products.product_id
+      JOIN scents
+        ON scents.id = scent_products.scent_id
+    WHERE volumes.volume_ml = '${volume}' 
+    AND scents.name = '${scent}';
+  `);
+};
+export default { findProducts, filterProducts };
